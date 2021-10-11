3667e6686c05f8d0d9ede1421ef4fd9cbbd4ae3b
Artyom Chumachenko <artyomchumachenko@mail.ru>
2021-10-11 20:59:41 +0300

    37 minutes later Stoped on animation for fish

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ ctx.font = '50px Georgia';
 
 // Mouse Interactivity
 let canvasPosition = canvas.getBoundingClientRect();
-// console.log(canvasPosition);
 
 const mouse = {
     x: canvas.width / 2,
@@ -21,16 +20,20 @@ canvas.addEventListener('mousedown', function (event) {
     mouse.click = true;
     mouse.x = event.x - canvasPosition.left;
     mouse.y = event.y - canvasPosition.top;
-    // console.log(mouse.x, mouse.y);
 });
 canvas.addEventListener('mouseup', function () {
     mouse.click = false;
 })
 
 // Player
+const playerLeft = new Image();
+playerLeft.src = 'fish_swim_left.png';
+const playerRight = new Image();
+playerRight.src = 'fish_swim_right.png';
+
 class Player {
     constructor() {
-        this.x = canvas.width / 2;
+        this.x = canvas.width;
         this.y = canvas.height / 2;
         this.radius = 50;
         this.angle = 0;
@@ -65,10 +68,87 @@ class Player {
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
         ctx.closePath();
+        ctx.fillRect(this.x, this.y, this.radius, 10);
     }
 }
+
 const player = new Player();
 
 // Bubbles
+const bubblesArray = [];
+
+class Bubble {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = canvas.height + 100;
+        this.radius = 50;
+        this.speed = Math.random() * 5 + 1;
+        this.distance;
+        this.counted = false;
+        this.sound = Math.random() <= 0.5 ? 'sound1' : 'sound2';
+    }
+
+    update() {
+        this.y -= this.speed;
+        const dx = this.x - player.x;
+        const dy = this.y - player.y;
+        this.distance = Math.sqrt(dx * dx + dy * dy);
+    }
+
+    draw() {
+        ctx.fillStyle = 'blue';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+
+const bubblePop1 = document.createElement('audio');
+bubblePop1.src = 'Plop.ogg';
+const bubblePop2 = document.createElement('audio');
+bubblePop2.src = 'bubbles-single1.wav';
+
+
+function handleBubbles() {
+    if (gameFrame % 50 == 0) {
+        bubblesArray.push(new Bubble());
+    }
+    for (let i = 0; i < bubblesArray.length; i++) {
+        bubblesArray[i].update();
+        bubblesArray[i].draw();
+    }
+    for (let i = 0; i < bubblesArray.length; i++) {
+        if (bubblesArray[i].y < 0 - bubblesArray[i].radius * 2) {
+            bubblesArray.splice(i, 1);
+        }
+        if (bubblesArray[i].distance < bubblesArray[i].radius + player.radius) {
+            if (!bubblesArray[i].counted) {
+                if (bubblesArray[i].sound == 'sound1') {
+                    bubblePop1.play();
+                } else {
+                    bubblePop2.play();
+                }
+                score++;
+                bubblesArray[i].counted = true;
+                bubblesArray.splice(i, 1);
+            }
+        }
+    }
+}
 
 // Animation Loop
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleBubbles();
+    player.update();
+    player.draw();
+    ctx.fillStyle = 'black';
+    ctx.fillText('score: ' + score, 10, 35);
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+
+animate();
