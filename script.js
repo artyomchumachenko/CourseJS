0588e0cb3735dba3f0451239a765e7ac0e6373c1
Artyom Chumachenko <artyomchumachenko@mail.ru>
2021-10-12 11:16:09 +0300

    Animation background

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ canvas.height = 500;
 let score = 0;
 let gameFrame = 0;
 ctx.font = '50px Georgia';
+let gameSpeed = 1;
 
 // Mouse Interactivity
 let canvasPosition = canvas.getBoundingClientRect();
@@ -47,6 +48,8 @@ class Player {
     update() {
         const dx = this.x - mouse.x;
         const dy = this.y - mouse.y;
+        let theta = Math.atan2(dy, dx);
+        this.angle = theta;
         if (mouse.x != this.x) {
             this.x -= dx / 30;
         }
@@ -69,6 +72,18 @@ class Player {
         ctx.fill();
         ctx.closePath();
         ctx.fillRect(this.x, this.y, this.radius, 10);
+
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        if (this.x >= mouse.x) {
+            ctx.drawImage(playerLeft, this.frameX = this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth,
+                this.spriteHeight, 0 - 60, 0 - 45, this.spriteWidth / 4, this.spriteHeight / 4);
+        } else {
+            ctx.drawImage(playerRight, this.frameX = this.spriteWidth, this.frameY * this.spriteHeight, this.spriteWidth,
+                this.spriteHeight, 0 - 60, 0 - 45, this.spriteWidth / 4, this.spriteHeight / 4);
+        }
+        ctx.restore();
     }
 }
 
@@ -96,7 +111,7 @@ class Bubble {
     }
 
     draw() {
-        ctx.fillStyle = 'blue';
+        ctx.fillStyle = 'orange';
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fill();
@@ -118,30 +133,50 @@ function handleBubbles() {
     for (let i = 0; i < bubblesArray.length; i++) {
         bubblesArray[i].update();
         bubblesArray[i].draw();
-    }
-    for (let i = 0; i < bubblesArray.length; i++) {
         if (bubblesArray[i].y < 0 - bubblesArray[i].radius * 2) {
             bubblesArray.splice(i, 1);
-        }
-        if (bubblesArray[i].distance < bubblesArray[i].radius + player.radius) {
+            i--;
+        } else if (bubblesArray[i].distance < bubblesArray[i].radius + player.radius) {
             if (!bubblesArray[i].counted) {
                 if (bubblesArray[i].sound == 'sound1') {
-                    bubblePop1.play();
+                    // bubblePop1.play();
                 } else {
-                    bubblePop2.play();
+                    // bubblePop2.play();
                 }
                 score++;
                 bubblesArray[i].counted = true;
                 bubblesArray.splice(i, 1);
+                i--;
             }
         }
     }
+}
+
+// Repeating backgrounds
+const background = new Image();
+background.src = 'cloud_background1.png';
+
+const BG = {
+    x1: 0,
+    x2: canvas.width,
+    y: -200,
+    width: canvas.width,
+    height: canvas.height,
+}
+
+function handleBackground() {
+    BG.x1--;
+    if (BG.x1 < -BG.width) {
+        BG.x1 = BG.width;
+    }
+    ctx.drawImage(background, BG.x1, BG.y, BG.width, BG.height);
 }
 
 // Animation Loop
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    handleBackground();
     handleBubbles();
     player.update();
     player.draw();
@@ -152,3 +187,7 @@ function animate() {
 }
 
 animate();
+
+window.addEventListener('resize', function () {
+    canvasPosition = canvas.getBoundingClientRect();
+});
